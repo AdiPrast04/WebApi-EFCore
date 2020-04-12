57ecc42904e33f091ea9b1003faabf9ec191383b
AdiPrast04 <Adiprasetyo0440@gmail.com>
2020-04-12 23:15:07 +0700

    updates

---
+++ WebApiEFCoreAngular/Models/UserDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLearningAngular04.Models
+{
+    public class UserDetails
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserName { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string UserNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(8)")]
+        public string JoinDate { get; set; }
+        [Required]
+        [Column(TypeName = "bit(0)")]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ WebApiEFCoreAngular/Models/UserDetailsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLearningAngular04.Models
+{
+    public class UserDetailsContext : DbContext
+    {
+        public UserDetailsContext(DbContextOptions<UserDetailsContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<UserDetails> UserDetails { get; set; }
+    }
+}
